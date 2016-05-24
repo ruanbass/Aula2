e9b9a44d667e864c19f929b0f1eeff152431ddc6
Ruan Rodrigues de Azevedo <ruan.rodrigues@DCX.UFPB.BR>
2016-05-24 11:11:22 -0300

    Termina em casa

--- src/main/java/dsc/pratica/UserManager.java
+++ src/main/java/dsc/pratica/UserManager.java
@@ -1,5 +1,24 @@
 package dsc.pratica;
 
-public class UserManager {
+import java.util.List;
 
+public class UserManager {
+	List<Usuario> usuarios;
+
+	public List<Usuario> getUsuarios() {
+		return usuarios;
+	}
+
+	public void setUsuarios(List<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+	public boolean validarUsuario(String nome,String senha){
+		for (int x = 0; x< usuarios.size(); x++){
+			if(usuarios.get(x).getNome() == nome && usuarios.get(x).getSenha() == senha ){
+				return true;
+			}
+			
+	}
+		return false;
+}
 }
---
+++ src/test/java/dsc/pratica/TesteUsuario.java
@@ -0,0 +1,7 @@
+package dsc.pratica;
+
+import junit.framework.TestCase;
+
+public class TesteUsuario extends TestCase {
+
+}
